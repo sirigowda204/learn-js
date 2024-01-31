cdc4fd06f99790499b6791c19042ebc4f8d1605d
Siri S <52484274+sirigowda204@users.noreply.github.com>
2024-01-31 14:51:47 -0500

    Incomplete

--- quiz/scripts/manipulatedom.js
+++ quiz/scripts/manipulatedom.js
@@ -21,11 +21,26 @@ function createTxtNode(txt) {
   return txtNode;
 }
 
+function createBtnNode(btnTxt, e, l) {
+  let btnNode = document.createElement("button");
+  return btnNode;
+}
+
 function addTable() {
   const tableNode = document.createElement("table");
   for(let i = 0; i < 3; i++) {
     let col1 = createTDNode(createTxtNode("Cell (" + i + ", 0)"));
+    let col2 = createTDNode(createBtnNode("Edit Text", "click", function() {
+      edit(col1);
+    }));
     tableNode.appendChild(createTRNode([col1]));
   }
   document.getElementById("root").appendChild(tableNode);
 }
+
+function edit(node){
+  let newNode = document.createElement("input");
+  newNode.type = 'text';
+  newNode.placeholder = "Enter cell (x,y) ...";
+}
+
